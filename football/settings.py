ed1a06fba32a13e13860bd8ca1e571b50ad234e7
officialksolomon <solomonuche42@gmail.com>
2021-11-08 08:51:31 +0100

    still on static files

--- football/settings.py
+++ football/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),
 #  Add configuration for static files storage using whitenoise
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
